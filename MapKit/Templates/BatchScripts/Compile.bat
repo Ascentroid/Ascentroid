6a8f2f1e45586b31650fd9913a02bfd553ab8bd8
Jonathan Vukovich <jon.vukovich@gmail.com>
2021-01-30 16:22:38 -0700

    - Bugfix: batch script directory issue when running as Windows Administrator
    - Updated documentation with some more info on UE4 version, requirement for .NET Framework Developer Pack, plus nicer formatting around sections.

--- MapKit/Templates/BatchScripts/Compile.bat
+++ MapKit/Templates/BatchScripts/Compile.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call Settings.bat
+call %~dp0\Settings.bat
 
 "%UE4_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%UPROJECT_FILE%" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -pak -stage -stagingdirectory="%PROJECT_PATH%\Saved\StagedBuilds"
 
