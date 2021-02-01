495e0986cb7e04c84b8de9daf35386dd3f8f442b
Jonathan Vukovich <jon.vukovich@gmail.com>
2021-01-31 22:20:40 -0700

    - Bugfix: needed to add quotes around call to Settings.bat

--- MapKit/Templates/BatchScripts/GenerateProject.bat
+++ MapKit/Templates/BatchScripts/GenerateProject.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call %~dp0\Settings.bat
+call "%~dp0\Settings.bat"
 
 "%UE4_PATH%\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%UPROJECT_FILE%" -game -rocket -progress -log="%PROJECT_PATH%\Saved\Logs\UnrealVersionSelector-Run-Manually.log"
 
