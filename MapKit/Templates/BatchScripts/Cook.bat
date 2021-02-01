e5514c595394ccd5772cadbab53e0ea844dd69f2
Jonathan Vukovich <jon.vukovich@gmail.com>
2021-01-31 23:37:08 -0700

    - Added a check to make sure Empty.umap exists

--- MapKit/Templates/BatchScripts/Cook.bat
+++ MapKit/Templates/BatchScripts/Cook.bat
@@ -2,6 +2,22 @@
 
 call "%~dp0\Settings.bat"
 
+if exist "%PROJECT_PATH%\Content\Empty.umap" (
+  goto :ok
+) else goto :emptylevelfilemissing
+
+:emptylevelfilemissing
+ECHO ERROR! ERROR! ERROR!
+echo "%PROJECT_PATH%\Content\Empty.umap" is missing!
+echo Re-download it from Github at: https://github.com/Ascentroid/Ascentroid/raw/latest-stable/MapKit/Templates/CampaignProject/Content/Empty.umap
+echo Save it here: "%PROJECT_PATH%\Content\Empty.umap"
+echo Re-run Cook.bat after you have the file put in the right place.
+echo IMPORTANT: Do not ever delete Empty.umap from the Content folder, or cook will not work!
+pause
+goto :end
+exit
+
+:ok
 rem Create the release
 start "1 Primary Cook" /wait /max /d "%UE4_PATH%\Engine\Build\BatchFiles\" RunUAT.bat -ScriptsForProject="%UPROJECT_FILE%" BuildCookRun -project="%UPROJECT_FILE%" -noP4 -clientconfig=Shipping -serverconfig=Shipping -nocompileeditor -ue4exe=UE4Editor-Cmd.exe -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -map=Empty -unversionedcookedcontent -pak -createreleaseversion=1.0 -compressed -stage -package -cmdline="Empty -Messaging" -addcmdline="-SessionId=0D0A64B94E0441EA86FFE9ADA0DD82DA -SessionOwner='jvukovich' -SessionName='Primary Cook' " -compile
 
@@ -32,3 +48,6 @@ echo Copying JSON from "%PROJECT_PATH%\%PROJECT_NAME%.json" to "%GAME_PATH%\%PRO
 copy /y "%PROJECT_PATH%\%PROJECT_NAME%.json" "%GAME_PATH%\%PROJECT_NAME%.json"
 
 pause
+
+:end
+exit
