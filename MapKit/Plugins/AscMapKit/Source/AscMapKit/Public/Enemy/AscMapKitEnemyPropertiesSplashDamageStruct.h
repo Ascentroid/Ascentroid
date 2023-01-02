dd971e303599a5119ae12876fdb56801f95dce28
Jonathan Vukovich <jon.vukovich@gmail.com>
2023-01-01 20:18:18 -0700

    - Update: new enemy properties

--- MapKit/Plugins/AscMapKit/Source/AscMapKit/Public/Enemy/AscMapKitEnemyPropertiesSplashDamageStruct.h
+++ MapKit/Plugins/AscMapKit/Source/AscMapKit/Public/Enemy/AscMapKitEnemyPropertiesSplashDamageStruct.h
@@ -11,23 +11,23 @@ struct ASCMAPKIT_API FAscMapKitEnemyPropertiesSplashDamageStruct
 {
     GENERATED_BODY()
 
-    // If enabled, splash damage will use a linear falloff calculation. This is negated if "Use Fixed Amount" is enabled.
+    // If enabled, splash damage to players/other enemies will use a linear falloff calculation. This is negated if "Use Fixed Amount" is enabled.
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     bool UseLinearFalloffAmount;
 
-    // The maximum amount of damage for the linear falloff calculation.
+    // The maximum amount of damage to players/other enemies for the linear falloff calculation.
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     FAscMapKitDifficultyFloatStruct LinearFalloffAmount;
 
-    // If enabled, splash damage will use a fixed damage amount. This overrides "Use Linear Falloff Amount".
+    // If enabled, splash damage to players/other enemies will use a fixed damage amount. This overrides "Use Linear Falloff Amount".
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     bool UseFixedAmount;
 
-    // The amount of damage for the fixed amount.
+    // The amount of damage to players/other enemies for the fixed amount.
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     FAscMapKitDifficultyFloatStruct FixedAmount;
 
-    // The radius used to apply the splash damage.
+    // The radius used to apply the splash damage to players/other enemies.
     // * 2000 is the size of a cube.
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     float ImpulseRadius;
@@ -47,4 +47,16 @@ struct ASCMAPKIT_API FAscMapKitEnemyPropertiesSplashDamageStruct
     // If the actor affected by the splash damage is an enemy, use this interpolation rate while applying the impulse effect. 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
     float ImpulseInterpSpeedAgainstEnemy;
+
+    // If the actor is hit by a weapon/projectile, and the weapon/projectile does not have splash damage specified, this is the default value used.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
+    float DefaultReceiveImpulseRadius;
+    
+    // If the actor is hit by a weapon/projectile, and the weapon/projectile does not have splash damage specified, this is the default value used.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
+    float DefaultReceiveImpulseStrength;
+
+    // If the actor is hit by a weapon/projectile, and the weapon/projectile does not have splash damage specified, this is the default value used.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame)
+    float DefaultReceiveImpulseInterpSpeed;
 };
