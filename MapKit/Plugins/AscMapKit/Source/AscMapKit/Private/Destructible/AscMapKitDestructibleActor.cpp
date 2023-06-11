270a35b304cdf605a2d8b253e340d8dd941e5ef7
Jonathan Vukovich <jon.vukovich@gmail.com>
2023-06-11 01:17:24 -0600

    - Update: debris damage by velocity settings

--- MapKit/Plugins/AscMapKit/Source/AscMapKit/Private/Destructible/AscMapKitDestructibleActor.cpp
+++ MapKit/Plugins/AscMapKit/Source/AscMapKit/Private/Destructible/AscMapKitDestructibleActor.cpp
@@ -11,14 +11,18 @@ AAscMapKitDestructibleActor::AAscMapKitDestructibleActor()
 
     MapKit.OverrideOnHitRadius = 5000.f;
     MapKit.OverrideOnHitImpulseStrength = 80000.f;
-    
+
     MapKit.PlayerDamage.Enable = true;
     MapKit.PlayerDamage.Amount = 3.f;
     MapKit.PlayerDamage.DelaySeconds = 1.f;
-    
+    MapKit.PlayerDamage.ScaleByVelocityMin = 1.f;
+	MapKit.PlayerDamage.ScaleByVelocityMax = 4500.f;
+
     MapKit.EnemyDamage.Enable = true;
     MapKit.EnemyDamage.Amount = 3.f;
     MapKit.EnemyDamage.DelaySeconds = 1.f;
+	MapKit.EnemyDamage.ScaleByVelocityMin = 1.f;
+	MapKit.EnemyDamage.ScaleByVelocityMax = 4500.f;
 
     MapKit.AllowDestroyByProjectile = true;
     MapKit.DestroyByProjectileStartShieldAmount = 30.f;
